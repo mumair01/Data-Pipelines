336090fe3643b7f84019afab331f0b7fc10472ce
Muhammad Umair <umair.aarij@gmail.com>
2022-08-21 15:11:56 -0400

    Removed init

--- data_pipelines/__init__.py
+++ data_pipelines/__init__.py
@@ -2,8 +2,5 @@
 # @Author: Muhammad Umair
 # @Date:   2022-07-08 15:59:39
 # @Last Modified by:   Muhammad Umair
-# @Last Modified time: 2022-08-21 14:18:33
+# @Last Modified time: 2022-08-21 15:11:09
 
-
-from . import datasets
-from . import features
