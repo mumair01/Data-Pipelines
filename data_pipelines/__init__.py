54290a647b81f4789ce3300173eb95ce0d555821
Muhammad Umair <umair.aarij@gmail.com>
2022-08-21 15:05:20 -0400

    Updates init

--- data_pipelines/__init__.py
+++ data_pipelines/__init__.py
@@ -2,7 +2,8 @@
 # @Author: Muhammad Umair
 # @Date:   2022-07-08 15:59:39
 # @Last Modified by:   Muhammad Umair
-# @Last Modified time: 2022-08-21 14:10:55
+# @Last Modified time: 2022-08-21 14:18:33
 
-import data_pipelines.datasets
-import data_pipelines.utils
+
+from . import datasets
+from . import features
