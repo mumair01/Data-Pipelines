86478007517b6a579165e013fa198ea57ee59e9d
Muhammad Umair <umair.aarij@gmail.com>
2023-08-22 10:36:24 -0600

    Added basic icc structure

---
+++ data_pipelines/datasets/icc/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Muhammad Umair
+# @Date:   2023-08-22 10:35:22
+# @Last Modified by:   Muhammad Umair
+# @Last Modified time: 2023-08-22 10:35:24
---
+++ data_pipelines/datasets/icc/download.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Muhammad Umair
+# @Date:   2023-08-22 10:35:30
+# @Last Modified by:   Muhammad Umair
+# @Last Modified time: 2023-08-22 10:36:13
+
+################
+# Contains the download class that is able to download a curated ICC corpus
+# from the TTS cloud storage set up for the Human Interaction Lab.
+################
