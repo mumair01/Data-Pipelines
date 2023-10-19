fcd407b2d5388eeb2289b732b1d22e4200574777
peter nadel <peter.m.nadel@gmail.com>
2023-10-19 11:27:03 -0400

    TTSClusterDownloader addition

--- data_pipelines/datasets/icc/download.py
+++ data_pipelines/datasets/icc/download.py
@@ -14,10 +14,11 @@ import shutil
 import os
 import json
 from typing import Type
+from collections.abc import Callable
 
 class GenericDownloader:
     """
-    Utility for downloading both the annotations and dialogues from useful corpera
+    Utility for downloading both the annotations and dialogues from useful corpora
     """
 
     def __init__(self, metadata_path: str, output_dir: str, force_download: bool = False) -> None:
@@ -46,8 +47,8 @@ class GenericDownloader:
             **paths
         )
 
-    def __download_files(self, download_func, **kwargs):
-        return download_func(**kwargs)
+    def __download_files(self, download_func: Callable, *args, **kwargs):
+        return download_func(*args, **kwargs)
 
     def __generate_dataclass(self) -> Type:
         fields = [(key, str) for key in self.metadata.keys()]
@@ -57,4 +58,36 @@ class GenericDownloader:
         return (
             os.path.exists(self.metadata_path)
         )
-    
+
+class TTSClusterDownloader(GenericDownloader):
+    """
+    Utility for downloading both the annotations and dialogues from useful corpora from the TTS Cluster
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        # authentication methods
+
+    def __call__(self, root_dir: str | os.PathLike, relative_paths: list[str | os.PathLike]):
+        DownloadPaths = self.__generate_dataclass()
+        paths_to_downloads = self.__get_paths_to_downloads(root_dir=root_dir, relative_paths=relative_paths)
+        pathsets = self.__download_folders(paths_to_downloads=paths_to_downloads)
+        return [DownloadPaths(**pathset) for pathset in pathsets]
+            
+    def __get_paths_to_downloads(self, root_dir: str | os.PathLike, relative_paths: list[str | os.PathLike]) -> list[str | os.PathLike]:
+        return [os.path.join(root_dir, rel_path) for rel_path in relative_paths]
+
+    def __download_func(self):
+        paths = self.download_paths # from metadata.json, should be a list of highest-level paths
+        for path in paths:
+            shutil.copytree(path, self.output_dir)
+
+    def __download_folders(self, paths_to_downloads: list[str | os.PathLike]) -> list[Type]:
+        return [self.__download_files(self.__download_func, path_to_downloads) for path_to_downloads in paths_to_downloads]
+
+    def upload_folder(self, folder_to_upload: str | os.PathLike, dest_paths: list[str | os.PathLike]):
+        for path in dest_paths:
+            shutil.copytree(folder_to_upload, path)
+
+    def upload_file(self, file_to_upload: str | os.PathLike, dest_paths: list[str | os.PathLike]):
+        for path in dest_paths:
+            shutil.copy2(file_to_upload, path)
